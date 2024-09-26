cff59391411628c4a79848f1e62d59cc5c40d4c6
bpdegnan <degnan.68k@gmail.com>
2024-09-26 15:11:56 -0400

    modified mtree/compare.c in order to support linux and bsd

--- src/mtree/compare.c
+++ src/mtree/compare.c
@@ -64,15 +64,15 @@ static char sccsid[] = "@(#)compare.c   8.1 (Berkeley) 6/6/93";
 #include "mtree.h"
 #include "extern.h"
 
-#define INDENTNAMELEN   8
-#define LABEL \
-        if (!label++) { \
+#define INDENTNAMELEN 8
+#define LABEL                                        \
+        if (!label++)                                \
+        {                                            \
                 len = printf("%s changed\n", RP(p)); \
-                tab = "\t"; \
+                tab = "\t";                          \
         }
 
-int
-compare(char *name, NODE *s, FTSENT *p)
+int compare(char *name, NODE *s, FTSENT *p)
 {
         struct timeval tv[2];
         uint32_t val;
@@ -85,7 +85,8 @@ compare(char *name, NODE *s, FTSENT *p)
 #endif
 
         label = 0;
-        switch(s->type) {
+        switch (s->type)
+        {
         case F_BLOCK:
                 if (!S_ISBLK(p->fts_statp->st_mode))
                         goto typeerr;
@@ -111,37 +112,41 @@ compare(char *name, NODE *s, FTSENT *p)
                         goto typeerr;
                 break;
         case F_SOCK:
-                if (!S_ISSOCK(p->fts_statp->st_mode)) {
-typeerr:                LABEL;
+                if (!S_ISSOCK(p->fts_statp->st_mode))
+                {
+                typeerr:
+                        LABEL;
                         (void)printf("\ttype expected %s found %s\n",
-                            ftype(s->type), inotype(p->fts_statp->st_mode));
+                                     ftype(s->type), inotype(p->fts_statp->st_mode));
                         return (label);
                 }
                 break;
         }
         /* Set the uid/gid first, then set the mode. */
-        if (s->flags & (F_UID | F_UNAME) && s->st_uid != p->fts_statp->st_uid) {
+        if (s->flags & (F_UID | F_UNAME) && s->st_uid != p->fts_statp->st_uid)
+        {
                 LABEL;
                 (void)printf("%suser expected %lu found %lu",
-                    tab, (u_long)s->st_uid, (u_long)p->fts_statp->st_uid);
+                             tab, (u_long)s->st_uid, (u_long)p->fts_statp->st_uid);
                 if (uflag)
                         if (chown(p->fts_accpath, s->st_uid, -1))
                                 (void)printf(" not modified: %s\n",
-                                    strerror(errno));
+                                             strerror(errno));
                         else
                                 (void)printf(" modified\n");
                 else
                         (void)printf("\n");
                 tab = "\t";
         }
-        if (s->flags & (F_GID | F_GNAME) && s->st_gid != p->fts_statp->st_gid) {
+        if (s->flags & (F_GID | F_GNAME) && s->st_gid != p->fts_statp->st_gid)
+        {
                 LABEL;
                 (void)printf("%sgid expected %lu found %lu",
-                    tab, (u_long)s->st_gid, (u_long)p->fts_statp->st_gid);
+                             tab, (u_long)s->st_gid, (u_long)p->fts_statp->st_gid);
                 if (uflag)
                         if (chown(p->fts_accpath, -1, s->st_gid))
                                 (void)printf(" not modified: %s\n",
-                                    strerror(errno));
+                                             strerror(errno));
                         else
                                 (void)printf(" modified\n");
                 else
@@ -150,14 +155,15 @@ typeerr:                LABEL;
         }
         if (s->flags & F_MODE &&
             !S_ISLNK(p->fts_statp->st_mode) &&
-            s->st_mode != (p->fts_statp->st_mode & MBITS)) {
+            s->st_mode != (p->fts_statp->st_mode & MBITS))
+        {
                 LABEL;
                 (void)printf("%spermissions expected %#o found %#o",
-                    tab, s->st_mode, p->fts_statp->st_mode & MBITS);
+                             tab, s->st_mode, p->fts_statp->st_mode & MBITS);
                 if (uflag)
                         if (chmod(p->fts_accpath, s->st_mode))
                                 (void)printf(" not modified: %s\n",
-                                    strerror(errno));
+                                             strerror(errno));
                         else
                                 (void)printf(" modified\n");
                 else
@@ -165,62 +171,93 @@ typeerr:                LABEL;
                 tab = "\t";
         }
         if (s->flags & F_NLINK && s->type != F_DIR &&
-            s->st_nlink != p->fts_statp->st_nlink) {
+            s->st_nlink != p->fts_statp->st_nlink)
+        {
                 LABEL;
                 (void)printf("%slink_count expected %u found %u\n",
-                    tab, (int)s->st_nlink, (int)p->fts_statp->st_nlink);
+                             tab, (int)s->st_nlink, (int)p->fts_statp->st_nlink);
                 tab = "\t";
         }
         if (s->flags & F_SIZE && s->st_size != p->fts_statp->st_size &&
-                !S_ISDIR(p->fts_statp->st_mode)) {
+            !S_ISDIR(p->fts_statp->st_mode))
+        {
                 LABEL;
                 (void)printf("%ssize expected %jd found %jd\n", tab,
-                    (intmax_t)s->st_size, (intmax_t)p->fts_statp->st_size);
+                             (intmax_t)s->st_size, (intmax_t)p->fts_statp->st_size);
                 tab = "\t";
         }
         /*
          * XXX
          * Catches nano-second differences, but doesn't display them.
          */
-        if ((s->flags & F_TIME) &&
-             ((s->st_mtimespec.tv_sec != p->fts_statp->st_mtimespec.tv_sec) ||
-             (s->st_mtimespec.tv_nsec != p->fts_statp->st_mtimespec.tv_nsec))) {
-                LABEL;
-                (void)printf("%smodification time expected %.24s ",
-                    tab, ctime(&s->st_mtimespec.tv_sec));
-                (void)printf("found %.24s",
-                    ctime(&p->fts_statp->st_mtimespec.tv_sec));
-                if (uflag) {
-                        tv[0].tv_sec = s->st_mtimespec.tv_sec;
-                        tv[0].tv_usec = s->st_mtimespec.tv_nsec / 1000;
-                        tv[1] = tv[0];
-                        if (utimes(p->fts_accpath, tv))
-                                (void)printf(" not modified: %s\n",
-                                    strerror(errno));
+        if ((s->flags & F_TIME))
+        {
+#ifdef __linux__
+                if ((s->st_mtimespec.tv_sec != p->fts_statp->st_mtim.tv_sec) ||
+                    (s->st_mtimespec.tv_nsec != p->fts_statp->st_mtim.tv_nsec))
+                {
+#else
+                if ((s->st_mtimespec.tv_sec != p->fts_statp->st_mtimespec.tv_sec) ||
+                    (s->st_mtimespec.tv_nsec != p->fts_statp->st_mtimespec.tv_nsec))
+                {
+#endif
+                        LABEL;
+                        (void)printf("%smodification time expected %.24s ",
+                                     tab, ctime(&s->st_mtimespec.tv_sec));
+#ifdef __linux__
+                        (void)printf("found %.24s",
+                                     ctime(&p->fts_statp->st_mtim.tv_sec));
+#else
+                        (void)printf("found %.24s",
+                                     ctime(&p->fts_statp->st_mtimespec.tv_sec));
+#endif
+                        if (uflag)
+                        {
+#ifdef __linux__
+                                tv[0].tv_sec = s->st_mtimespec.tv_sec;
+                                tv[0].tv_usec = s->st_mtimespec.tv_nsec / 1000;
+                                tv[1] = tv[0];
+#else
+                                tv[0].tv_sec = s->st_mtimespec.tv_sec;
+                                tv[0].tv_usec = s->st_mtimespec.tv_nsec / 1000;
+                                tv[1] = tv[0];
+#endif
+                                if (utimes(p->fts_accpath, tv))
+                                        (void)printf(" not modified: %s\n", strerror(errno));
+                                else
+                                        (void)printf(" modified\n");
+                        }
                         else
-                                (void)printf(" modified\n");
-                } else
-                        (void)printf("\n");
-                tab = "\t";
+                                (void)printf("\n");
+                        tab = "\t";
+                }
         }
-        if (s->flags & F_CKSUM) {
-                if ((fd = open(p->fts_accpath, O_RDONLY, 0)) < 0) {
+
+        if (s->flags & F_CKSUM)
+        {
+                if ((fd = open(p->fts_accpath, O_RDONLY, 0)) < 0)
+                {
                         LABEL;
                         (void)printf("%scksum: %s: %s\n",
-                            tab, p->fts_accpath, strerror(errno));
+                                     tab, p->fts_accpath, strerror(errno));
                         tab = "\t";
-                } else if (crc(fd, &val, &len)) {
+                }
+                else if (crc(fd, &val, &len))
+                {
                         (void)close(fd);
                         LABEL;
                         (void)printf("%scksum: %s: %s\n",
-                            tab, p->fts_accpath, strerror(errno));
+                                     tab, p->fts_accpath, strerror(errno));
                         tab = "\t";
-                } else {
+                }
+                else
+                {
                         (void)close(fd);
-                        if (s->cksum != val) {
+                        if (s->cksum != val)
+                        {
                                 LABEL;
                                 (void)printf("%scksum expected %lu found %lu\n",
-                                    tab, s->cksum, (unsigned long)val);
+                                             tab, s->cksum, (unsigned long)val);
                                 tab = "\t";
                         }
                 }
@@ -248,16 +285,20 @@ typeerr:                LABEL;
         }
 #endif
 #if HAVE_OPENSSL_MD5_H && HAVE_EVP_MD5
-        if (s->flags & F_MD5) {
-                char *new_digest, result[MD5_DIGEST_LENGTH*2+1];
+        if (s->flags & F_MD5)
+        {
+                char *new_digest, result[MD5_DIGEST_LENGTH * 2 + 1];
 
                 new_digest = MD5_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sMD5: %s: %s\n", tab, p->fts_accpath,
                                strerror(errno));
                         tab = "\t";
-                } else if (strcmp(new_digest, s->md5digest)) {
+                }
+                else if (strcmp(new_digest, s->md5digest))
+                {
                         LABEL;
                         printf("%sMD5 expected %s found %s\n", tab, s->md5digest,
                                new_digest);
@@ -266,16 +307,20 @@ typeerr:                LABEL;
         }
 #endif /* HAVE_OPENSSL_MD5_H */
 #if HAVE_OPENSSL_SHA_H && HAVE_EVP_SHA1
-        if (s->flags & F_SHA1) {
-                char *new_digest, result[SHA_DIGEST_LENGTH*2+1];
+        if (s->flags & F_SHA1)
+        {
+                char *new_digest, result[SHA_DIGEST_LENGTH * 2 + 1];
 
                 new_digest = SHA1_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sSHA-1: %s: %s\n", tab, p->fts_accpath,
                                strerror(errno));
                         tab = "\t";
-                } else if (strcmp(new_digest, s->sha1digest)) {
+                }
+                else if (strcmp(new_digest, s->sha1digest))
+                {
                         LABEL;
                         printf("%sSHA-1 expected %s found %s\n",
                                tab, s->sha1digest, new_digest);
@@ -284,16 +329,20 @@ typeerr:                LABEL;
         }
 #endif /* HAVE_OPENSSL_SHA_H */
 #if HAVE_OPENSSL_RIPEMD_H && HAVE_EVP_RIPEMD160
-        if (s->flags & F_RMD160) {
-                char *new_digest, result[RIPEMD160_DIGEST_LENGTH*2+1];
+        if (s->flags & F_RMD160)
+        {
+                char *new_digest, result[RIPEMD160_DIGEST_LENGTH * 2 + 1];
 
                 new_digest = RIPEMD160_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sRIPEMD160: %s: %s\n", tab,
                                p->fts_accpath, strerror(errno));
                         tab = "\t";
-                } else if (strcmp(new_digest, s->rmd160digest)) {
+                }
+                else if (strcmp(new_digest, s->rmd160digest))
+                {
                         LABEL;
                         printf("%sRIPEMD160 expected %s found %s\n",
                                tab, s->rmd160digest, new_digest);
@@ -302,16 +351,20 @@ typeerr:                LABEL;
         }
 #endif /* HAVE_OPENSSL_RIPEMD_H */
 #if HAVE_OPENSSL_SHA_H && HAVE_EVP_SHA256
-        if (s->flags & F_SHA256) {
-                char *new_digest, result[SHA256_DIGEST_LENGTH*2+1];
+        if (s->flags & F_SHA256)
+        {
+                char *new_digest, result[SHA256_DIGEST_LENGTH * 2 + 1];
 
                 new_digest = SHA256_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sSHA-256: %s: %s\n", tab, p->fts_accpath,
                                strerror(errno));
                         tab = "\t";
-                } else if (strcmp(new_digest, s->sha256digest)) {
+                }
+                else if (strcmp(new_digest, s->sha256digest))
+                {
                         LABEL;
                         printf("%sSHA-256 expected %s found %s\n",
                                tab, s->sha256digest, new_digest);
@@ -320,17 +373,21 @@ typeerr:                LABEL;
         }
 #endif
 #if HAVE_OPENSSL_SHA_H && HAVE_EVP_SHA384
-        if (s->flags & F_SHA384) {
-                char *new_digest, result[SHA384_DIGEST_LENGTH*2+1];
+        if (s->flags & F_SHA384)
+        {
+                char *new_digest, result[SHA384_DIGEST_LENGTH * 2 + 1];
 
-		// XXX(vbatts) really not sure why SHA384_File is not returning the correct char*
+                // XXX(vbatts) really not sure why SHA384_File is not returning the correct char*
                 new_digest = SHA384_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sSHA-384: %s: %s\n", tab, p->fts_accpath,
                                strerror(errno));
                         tab = "\t";
-                } else if (strcmp(result, s->sha384digest)) {
+                }
+                else if (strcmp(result, s->sha384digest))
+                {
                         LABEL;
                         printf("%sSHA-384 expected %s found %s\n",
                                tab, s->sha384digest, result);
@@ -339,16 +396,20 @@ typeerr:                LABEL;
         }
 #endif
 #if HAVE_OPENSSL_SHA_H && HAVE_EVP_SHA512
-        if (s->flags & F_SHA512) {
-                char *new_digest, result[SHA512_DIGEST_LENGTH*2+1];
+        if (s->flags & F_SHA512)
+        {
+                char *new_digest, result[SHA512_DIGEST_LENGTH * 2 + 1];
 
                 new_digest = SHA512_File(p->fts_accpath, result);
-                if (!new_digest) {
+                if (!new_digest)
+                {
                         LABEL;
                         printf("%sSHA-512: %s: %s\n", tab, p->fts_accpath,
                                strerror(errno));
                         tab = "\t";
-                } else if (strcmp(new_digest, s->sha512digest)) {
+                }
+                else if (strcmp(new_digest, s->sha512digest))
+                {
                         LABEL;
                         printf("%sSHA-512 expected %s found %s\n",
                                tab, s->sha512digest, new_digest);
@@ -358,10 +419,11 @@ typeerr:                LABEL;
 #endif /* HAVE_OPENSSL_SHA_H */
 
         if (s->flags & F_SLINK &&
-            strcmp(cp = rlink(p->fts_accpath), s->slink)) {
+            strcmp(cp = rlink(p->fts_accpath), s->slink))
+        {
                 LABEL;
                 (void)printf("%slink_ref expected %s found %s\n",
-                      tab, s->slink, cp);
+                             tab, s->slink, cp);
         }
         return (label);
 }
@@ -369,7 +431,8 @@ typeerr:                LABEL;
 const char *
 inotype(u_int type)
 {
-        switch(type & S_IFMT) {
+        switch (type & S_IFMT)
+        {
         case S_IFBLK:
                 return ("block");
         case S_IFCHR:
@@ -393,7 +456,8 @@ inotype(u_int type)
 const char *
 ftype(u_int type)
 {
-        switch(type) {
+        switch (type)
+        {
         case F_BLOCK:
                 return ("block");
         case F_CHAR:
